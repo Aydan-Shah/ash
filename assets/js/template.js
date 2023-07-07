4a590f367a08531bd5cc6a1983b305b7ef22b6fb
Aydan <aydanhilal5@gamil.com>
2023-07-07 22:45:09 +0400

    first commit

---
+++ assets/js/template.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Smart"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-3", {
+        strings: ["Intelligent"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-4", {
+        strings: ["Powerful"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-5", {
+        strings: ["Helper"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+})
